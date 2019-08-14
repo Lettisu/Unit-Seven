2c960ef86ab65587785a108b17bde8931ba814dc
Lettisu <51244169+Lettisu@users.noreply.github.com>
2019-08-14 08:25:43 -0400

    Update NotFound.js

--- my-app/src/components/NotFound.js
+++ my-app/src/components/NotFound.js
@@ -3,7 +3,7 @@ const NotFound = () => {
     return (
         <div className="Not Found">
             <h3> "No results found" </h3>
-            <p> "404 error Page not found" </p>
+            <p> "404 error Page not Found" </p>
         </div>)
 }
 export default NotFound;
